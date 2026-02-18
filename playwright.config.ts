9e3a38d5cdea66e7601df0b887a02a8756f0b718
Anton Nesterov <sbs-06@mail.ru>
2026-02-18 10:54:47 +0300

    use POM

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,32 +12,33 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './tests', // Директория с тестами.
   snapshotPathTemplate: '{testDir}/screenshots/{testFilePath}/{arg}{ext}',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: true, // Запуск тестов в параллельном режиме.
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: !!process.env.CI, // Запрет на оставление тестов с test.only.
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 0, // Количество попыток перезапуска на CI.
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : undefined, // Количество воркеров для параллельных тестов.
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'html', // Репортер для генерации отчётов.
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000', // Базовый URL для тестов.
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    screenshot: 'only-on-failure', // Скриншот только при сбое.
+    trace: 'on-first-retry', // Формирование trace при первом перезапуске.
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'] }, // Конфигурация для Chrome.
     },
 
     // {
@@ -73,8 +74,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
+  //   command: 'npm run start', // Команда для запуска локального сервера.
+  //   url: 'http://localhost:3000', // URL локального сервера.
+  //   reuseExistingServer: !process.env.CI, // Повторное использование сервера.
   // },
 });
